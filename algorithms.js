f922569bebc7bb82306e774320caf8642b8c4147
jqn1 <154387579+jqn1@users.noreply.github.com>
2025-04-09 21:08:57 -0400

    fix undefinded variables in bubbleSort

--- algorithms.js
+++ algorithms.js
@@ -1,11 +1,11 @@
 function bubbleSort(array){
     for (let i = 0; i < array.length - 1; i++){
-        for (let j = 0; j < array.length - i; j++){
-            console.log("i = ",i,"j = ",j,"\n array: ",array);
+        for (let j = 0; j < array.length - (i + 1); j++){
+            //console.log("i = ",i,"j = ",j,"\n array: ",array);
             if (array[j] > array[j+1]){
-                let aux = arr[j];
-                arr[j] = arr[j+1];
-                arr[j+1] = aux;
+                let aux = array[j];
+                array[j] = array[j+1];
+                array[j+1] = aux;
             }
         }
     }
